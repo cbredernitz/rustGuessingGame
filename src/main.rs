bc61b7d9b6413fb6c0ee03a6ba8dbf9a18121885
Chris <chrisrbredernitz@gmail.com>
2023-12-05 22:32:09 -0700

    add the print of a random guess. using thread_rng in order to use the random number generator that is thread local to the execution.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Your secret number is: {secret_number}");
 
     let mut guess = String::new();
 
