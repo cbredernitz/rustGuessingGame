107e25822a9f00ba9789c4a5b67bb92111bd8655
Chris <chrisrbredernitz@gmail.com>
2023-12-07 18:39:52 -0700

    added a matcher to the guess with ok and err arms. wrapped all in a loop where it will have users input a guess unill there is a match

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,32 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess = String::new();
 
-    println!("Your secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read this line");
     
-    io::stdin().read_line(&mut guess).expect("failed to read this line");
-
-    let guess: u32 = guess.trim().parse().expect("Please Type a Number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small :("),
-        Ordering::Equal => println!("You Win!!!!"),
-        Ordering::Greater => println!("Too Big :(")
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small :("),
+            Ordering::Greater => println!("Too Big :("),
+            Ordering::Equal => {
+                println!("You Win!!!!");
+                break;
+            },
+        }
     }
 }
