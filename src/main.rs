62fa2382d1bb78f8477dbb1688ee8f401831ae1e
Chris <chrisrbredernitz@gmail.com>
2023-12-07 18:27:30 -0700

    matcher on guess string added. Compares guess against the secret number randomized. Converted the guess String to a u32 integer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, cmp::Ordering};
 
 use rand::Rng;
 
@@ -7,12 +7,19 @@ fn main() {
     println!("Please input your guess");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("Your secret number is: {secret_number}");
-
     let mut guess = String::new();
 
+    println!("Your secret number is: {secret_number}");
+    
     io::stdin().read_line(&mut guess).expect("failed to read this line");
 
+    let guess: u32 = guess.trim().parse().expect("Please Type a Number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small :("),
+        Ordering::Equal => println!("You Win!!!!"),
+        Ordering::Greater => println!("Too Big :(")
+    }
 }
